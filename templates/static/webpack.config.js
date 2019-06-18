5697d6087a81b0188cfcf8939782b49ed780299d
Kai Kuspa <kaikuspa@Kais-MBP.fios-router.home>
2019-06-18 02:36:55 -0400

    working react, flask, npm, and endpoints

--- templates/static/webpack.config.js
+++ templates/static/webpack.config.js
@@ -16,10 +16,11 @@ const config = {
     {
      test: /\.jsx?/,
      loader: 'babel-loader',
-     exclude: /node_modules/,
-     query:{
-        presets: ['react','es2015']
-      }
+     exclude: /node_modules/
+    },
+    {
+    test: /\.css$/,
+    loader: 'style-loader!css-loader?modules'
     }]
    }
 };
